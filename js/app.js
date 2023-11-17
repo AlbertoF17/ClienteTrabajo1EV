5b6568ee5da24611d500db618fb64730a6976137
AlbertoF17 <albertofergom@gmail.com>
2023-11-17 20:54:15 +0100

    Control de inicio de sesión

--- js/app.js
+++ js/app.js
@@ -191,17 +191,17 @@ if (window.location.pathname.endsWith("session.html")) {
 
     function login(event) {
         event.preventDefault(); // Prevenir el envío del formulario por defecto
-
+    
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-
+    
         // Verificar si el usuario existe en el LocalStorage
         const existingUser = localStorage.getItem('user_' + username);
-
+    
         if (existingUser) {
             // Parsear el JSON al recuperar el usuario del localStorage
             const parsedUser = JSON.parse(existingUser);
-
+    
             // Verificar si el usuario tiene la propiedad 'address'
             if ('address' in parsedUser) {
                 const currentUser = new User(
@@ -224,9 +224,42 @@ if (window.location.pathname.endsWith("session.html")) {
                 window.location.href = "../index.html";
             }
         } else {
-            alert('Inicio de sesión fallido. Verifica tu usuario y contraseña.');
+            // Si el usuario no está en el localStorage, intentar autenticar a través de la API
+            authenticateUser(username, password)
+                .then(user => {
+                    if (user) {
+                        // Autenticación exitosa, guardar en el localStorage y redirigir
+                        localStorage.setItem(`user_${user.username}`, JSON.stringify(user));
+                        sessionStorage.setItem('user', JSON.stringify(user));
+                        window.location.href = "../index.html";
+                    } else {
+                        alert('Inicio de sesión fallido. Verifica tu usuario y contraseña.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error al autenticar usuario:', error);
+                    alert('Error al autenticar usuario. Por favor, inténtelo de nuevo.');
+                });
         }
     }
+    
+    // Función para autenticar al usuario a través de la API
+    async function authenticateUser(username, password) {
+        try {
+            const res = await fetch('https://fakestoreapi.com/auth/login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            });
+            const json = await res.json();
+            return json;
+        } catch (error) {
+            console.error('Error en la autenticación del usuario:', error);
+        }
+    }
+    
     document.getElementById("login-form").addEventListener("submit", login);
     document.getElementById("register-form").addEventListener("submit", register);
 }
