3e27db1c4175faeb6d163c054c5dd4484ecd3b57
AlbertoF17 <albertofergom@gmail.com>
2023-11-27 00:11:18 +0100

    Filtrado de Productos

--- js/app.js
+++ js/app.js
@@ -50,8 +50,115 @@ class Product {
 
 if (window.location.pathname.endsWith("index.html")) {
     checkLoggedIn();
-    fetchCategories();
-    displayProducts(fetchProducts());
+
+    const categorySelector = document.querySelector("#category-select");
+    const productsContainer = document.querySelector("#products-container");
+    const cartButton = document.querySelector("#cart-dropdown-btn");
+    const cartDropdown = document.querySelector("#cart-dropdown");
+
+
+    cartButton.addEventListener("click", function () {
+        cartDropdown.classList.toggle("active");
+        const cartItems = JSON.parse(sessionStorage.getItem("cart"));
+        cartDropdown.innerHTML = '';
+        if (cartItems && cartItems.length > 0) {
+            cartItems.forEach(product => {
+                const productDiv = document.createElement("div");
+                const quantityDiv = document.createElement("div");
+                const buttonPlus = document.createElement("button");
+                const buttonMinus = document.createElement("button");
+                const productImg = document.createElement("img");
+                const productName = document.createElement("p");
+                const productPrice = document.createElement("p");
+                const productQuantity = document.createElement("p");
+                productName.textContent = product.title;
+                productName.style.fontWeight = "bold";
+                productPrice.textContent = product.price;
+                productQuantity.textContent = product.quantity;
+                productImg.src = product.image;
+                quantityDiv.style.display = "flex";
+                quantityDiv.style.flexDirection = "column";
+                quantityDiv.style.alignItems = "center";
+                quantityDiv.style.textAlign = "center";
+                buttonPlus.textContent = "+";
+                buttonPlus.style.width = "1.5rem";
+                buttonPlus.addEventListener("click", () => updateQuantity(product.id, 1));
+                buttonPlus.className = "quantity-button";
+                buttonMinus.textContent = "-";
+                buttonMinus.style.width = "1.5rem";
+                buttonMinus.addEventListener("click", () => updateQuantity(product.id, -1));
+                buttonMinus.className = "quantity-button";
+                productDiv.appendChild(productImg);
+                productDiv.appendChild(productName);
+                productDiv.appendChild(productPrice);
+                quantityDiv.appendChild(buttonPlus);
+                quantityDiv.appendChild(productQuantity);
+                quantityDiv.appendChild(buttonMinus);
+                productDiv.appendChild(quantityDiv);
+                productDiv.className = "cart-product";
+                productDiv.style.display = "flex";
+                cartDropdown.appendChild(productDiv);
+            })
+            const confirmPurchase = document.createElement("button");
+            confirmPurchase.id = "confirm-purchase-btn";
+            confirmPurchase.textContent = "Confirm Purchase";
+            cartDropdown.appendChild(confirmPurchase);
+        } else {
+            cartDropdown.textContent = "Empty Cart";
+        }
+    });
+
+    function updateQuantity(productId, quantityChange) {
+        try {
+            const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+            const existingProductIndex = cart.findIndex(p => p.id === productId);
+
+            if (existingProductIndex !== -1) {
+
+                cart[existingProductIndex].quantity += quantityChange;
+
+                if (cart[existingProductIndex].quantity === 0) {
+                    cart.splice(existingProductIndex, 1);
+                }
+
+                sessionStorage.setItem("cart", JSON.stringify(cart));
+
+                cartButton.click();
+                cartButton.click();
+            }
+        } catch (error) {
+            console.error("Error al actualizar la cantidad:", error);
+        }
+    }
+
+    function toggleDropdown() {
+        var dropdownContent = document.getElementById("dropdown-content");
+        dropdownContent.classList.toggle("show");
+    }
+
+    function goToProfile() {
+        // Implementar la lógica para redirigir al perfil
+        console.log("Ir al perfil");
+    }
+
+    // Cerrar el menú desplegable si el usuario hace clic fuera de él
+    window.onclick = function (event) {
+        if (!event.target.matches('.profile-image')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
+    categorySelector.addEventListener("change", filterProductsByCategory);
+
+    displayCategories(categorySelector);
+    filterProductsByCategory();
 
     function checkLoggedIn() {
         const currentUser = sessionStorage.getItem("user");
@@ -59,61 +166,6 @@ if (window.location.pathname.endsWith("index.html")) {
             window.location.href = "pages/session.html";
         }
     }
-
-    async function fetchCategories() {
-        try {
-            const response = await fetch("https://fakestoreapi.com/products/categories");
-            const categories = await response.json();
-    
-            const selectElement = document.querySelector("#category-select");
-    
-            categories.forEach(category => {
-                const option = document.createElement("option");
-                option.value = category;
-                option.text = category;
-                selectElement.appendChild(option);
-            });
-    
-            selectElement.addEventListener("change", function() {
-                const selectedCategory = selectElement.value;
-    
-                fetch(`https://fakestoreapi.com/products/category/${selectedCategory}`)
-                    .then(res => res.json())
-                    .then(json => console.log(json))
-                    .catch(error => console.error("Error fetching products:", error));
-            });
-    
-            return categories; // Devuelve las categorías
-        } catch (error) {
-            console.error("Error fetching categories:", error);
-            return []; // Devuelve un array vacío en caso de error
-        }
-    }
-
-    async function filterProductsByCategory() {
-        const selectElement = document.querySelector("#category-select");
-        const selectedCategory = selectElement.value;
-    
-        const productsContainer = document.querySelector("#products-container");
-        productsContainer.innerHTML = ""; // Limpiar el contenedor antes de agregar los productos filtrados
-    
-        if (selectedCategory === "all") {
-            // Si se selecciona la opción "all", mostrar todos los productos
-            const allProducts = await fetchProducts();
-            displayProducts(allProducts);
-        } else {
-            // Filtrar productos por la categoría seleccionada
-            try {
-                const response = await fetch(`https://fakestoreapi.com/products/category/${selectedCategory}`);
-                const filteredProducts = await response.json();
-                displayProducts(filteredProducts);
-            } catch (error) {
-                console.error("Error fetching filtered products:", error);
-            }
-        }
-    }
-    
-    
 
     async function fetchProducts() {
         try {
@@ -140,26 +192,106 @@ if (window.location.pathname.endsWith("index.html")) {
     }
 
     async function displayProducts(products) {
-        const productsContainer = document.querySelector("#products-container");
-        products = await fetchProducts();
+        productsContainer.innerHTML = "";
         products.forEach(product => {
             const productDiv = document.createElement("div");
             const productName = document.createElement("h3");
             const productImg = document.createElement("img");
             const productDesc = document.createElement("p");
+            const btnDiv = document.createElement("div");
+            const addToCartBtn = document.createElement("button");
+            const viewProductBtn = document.createElement("button");
 
             productDiv.className = "product";
             productName.textContent = product.title;
             productImg.src = product.image;
             productDesc.textContent = product.description;
+            addToCartBtn.textContent = "Add to Cart";
+            viewProductBtn.textContent = "View Product";
+            btnDiv.className = "product-btns";
+            addToCartBtn.addEventListener("click", () => addToCart(product));
+            viewProductBtn.addEventListener("click", () => viewProduct(product.id));
 
+            btnDiv.appendChild(addToCartBtn);
+            btnDiv.appendChild(viewProductBtn);
             productDiv.appendChild(productName);
             productDiv.appendChild(productImg);
             productDiv.appendChild(productDesc);
+            productDiv.appendChild(btnDiv);
 
             productsContainer.appendChild(productDiv);
         });
     }
+
+    async function fetchCategories() {
+        const response = await fetch('https://fakestoreapi.com/products/categories');
+        const apiCategories = await response.json();
+        const localCategories = localStorage.getItem("localCategories") || [];
+        const uniqueApiCategories = apiCategories.filter(apiCategory =>
+            !localCategories.some(localCategory => localCategory.id === apiCategory.id)
+        );
+
+        const allCategories = [...uniqueApiCategories, ...localCategories];
+        return allCategories;
+    }
+
+    async function displayCategories(categorySelector) {
+        const categoriesList = await fetchCategories();
+        categoriesList.forEach(category => {
+            const categoryOption = document.createElement("option");
+            categoryOption.value = category;
+            categoryOption.textContent = category;
+            categorySelector.appendChild(categoryOption);
+        });
+    }
+
+    async function filterProductsByCategory() {
+        const selectedCategory = categorySelector.value;
+        const products = await fetchProducts();
+        const filteredProducts = [];
+
+        products.forEach(product => {
+            if (selectedCategory === "all") {
+                filteredProducts.push(product);
+            } else if (product.category === selectedCategory) {
+                filteredProducts.push(product);
+            }
+        });
+
+        displayProducts(filteredProducts);
+    }
+
+    async function viewProduct(productId) {
+        try {
+            const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+            const product = await response.json();
+    
+            sessionStorage.setItem('currentProduct', JSON.stringify(product));
+            window.location.href = `pages/product.html?id=${productId}`;
+        } catch (error) {
+            console.error("Error fetching product:", error);
+        }
+    }
+
+    function addToCart(product) {
+        try {
+            const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+            const existingProductIndex = cart.findIndex(p => p.id === product.id);
+
+            if (existingProductIndex !== -1) {
+                cart[existingProductIndex].quantity += 1;
+            } else {
+                // Agrega el producto al carrito con cantidad 1
+                cart.push({ ...product, quantity: 1 });
+            }
+
+            sessionStorage.setItem("cart", JSON.stringify(cart));
+            console.log(`Producto "${product.title}" agregado al carrito.`);
+        } catch (error) {
+            console.error("Error al añadir al carrito:", error);
+        }
+    }
+
 
     document.getElementById("logout-button").addEventListener("click", function () {
         // Eliminar al usuario del sessionStorage
@@ -168,15 +300,6 @@ if (window.location.pathname.endsWith("index.html")) {
         // Redirigir a la página de inicio de sesión u otra acción después de cerrar sesión
         window.location.href = "pages/session.html";
     });
-
-    document.querySelector("#login-form").addEventListener("submit", login);
-    document.querySelector("#register-form").addEventListener("submit", register);
-    document.querySelector("#category-select").addEventListener("change", filterProductsByCategory);
-    document.querySelector("#add-product-form").addEventListener("submit", function (event) {
-        event.preventDefault();
-        //addProduct();
-    });
-    document.querySelector("#confirm-purchase-btn").addEventListener("click", confirmPurchase);
 
 }
 
@@ -250,17 +373,17 @@ if (window.location.pathname.endsWith("session.html")) {
 
     function login(event) {
         event.preventDefault(); // Prevenir el envío del formulario por defecto
-    
+
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-    
+
         // Verificar si el usuario existe en el LocalStorage
         const existingUser = localStorage.getItem('user_' + username);
-    
+
         if (existingUser) {
             // Parsear el JSON al recuperar el usuario del localStorage
             const parsedUser = JSON.parse(existingUser);
-    
+
             // Verificar si el usuario tiene la propiedad 'address'
             if ('address' in parsedUser) {
                 const currentUser = new User(
@@ -301,7 +424,7 @@ if (window.location.pathname.endsWith("session.html")) {
                 });
         }
     }
-    
+
     // Función para autenticar al usuario a través de la API
     async function authenticateUser(username, password) {
         try {
@@ -318,4 +441,48 @@ if (window.location.pathname.endsWith("session.html")) {
             console.error('Error en la autenticación del usuario:', error);
         }
     }
+
+    document.getElementById("login-form").addEventListener("submit", login);
+    document.getElementById("register-form").addEventListener("submit", register);
+}
+
+const currentProduct = JSON.parse(sessionStorage.getItem('currentProduct'));
+if (currentProduct) {
+    productContent(currentProduct);
+} else {
+    console.error("Product details not found.");
+}
+
+function productContent(product) {
+    const productDiv = document.querySelector("#product-details");
+    const productInfo = document.querySelector("#product-info");
+    const productTitle = document.createElement("h3");
+    const productImage = document.createElement("img");
+    const productPrice = document.createElement("p");
+    const productCategory = document.createElement("p");
+    const productDescription = document.createElement("p");
+    const productEditButton = document.createElement("button");
+    const productDeleteButton = document.createElement("button");
+    productTitle.textContent = `${product.title}`;
+    productImage.src = `${product.image}`;
+    productPrice.textContent = `${product.price} €`;
+    productCategory.textContent = `${product.category}`;
+    productDescription.textContent = `${product.description}`;
+    productEditButton.textContent = `Edit`;
+    productDeleteButton.textContent = `Delete`;
+    productInfo.appendChild(productTitle);
+    productInfo.appendChild(productImage);
+    productInfo.appendChild(productPrice);
+    productInfo.appendChild(productCategory);
+    productInfo.appendChild(productDescription);
+    productInfo.appendChild(productButton);
+    productDiv.style.display="flex";
+    productDiv.style.flexDirection = "column";
+    productDiv.style.alignItems = "center";
+    productInfo.style.display="flex";
+    productInfo.style.flexDirection = "column";
+    productInfo.style.alignItems = "center";
+    productInfo.style.textAlign = "center";
+    productInfo.style.width = "70vw";
+    productImage.style.height = "50vh";
 }
